5ab60d227e38fa68e15561578c0430e0591aab26
Pamela Castaneda <pamelacastanedav@gmail.com>
2022-05-24 11:21:21 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,11 @@
+//PRACTICE PREDICTION
+
+///1,2,3,4,5,6,7,8,9
+//it does not go thorugh 10 bc it breaks before completing the loop
+
+for (var i = 1; i < 100; i++) {
+  if (i == 10) {
+    break;
+  }
+  console.log(i);
+}
