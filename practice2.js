5a2a5a95864537767e0df61cc2abdc31816f47ee
Pamela Castaneda <pamelacastanedav@gmail.com>
2022-05-26 10:09:50 -0700

    commit

---
+++ practice2.js
@@ -0,0 +1,10 @@
+// PREDICTION
+
+//i starts at 13, loop will only go through 13 -20-= it will not run through 50
+
+for (var i = 13; i <= 20; i++) {
+  if (i == 50) {
+    break;
+  }
+  console.log(i);
+}
