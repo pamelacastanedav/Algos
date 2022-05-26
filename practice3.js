bd5da32524f391b6f4035edec292e8e1a12556ba
Pamela Castaneda <pamelacastanedav@gmail.com>
2022-05-26 10:15:05 -0700

    commit

---
+++ practice3.js
@@ -0,0 +1,12 @@
+// PREDICTION
+//i starts at 1, will only run through 20
+//it will break at 5
+// hello x 5
+
+for (var i = 1; i <= 20; i++) {
+  if (i > 5) {
+    break; //if it is greater than 5 it will break else ignores it.
+  } else {
+    console.log("hello");
+  }
+}
