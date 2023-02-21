e0462ab337ce48297ee5de499d232c4803a3a07e
Pamela Castaneda <pamelacastanedav@gmail.com>
2023-02-21 10:20:41 -0800

    commit

---
+++ practice4.js
@@ -0,0 +1,30 @@
+var counter = 1;
+if (counter == 1) {
+  console.log("hello");
+} else {
+  counter == 2;
+
+  console.log("world");
+}
+
+var number = 3;
+if (number < 10) {
+  console.log("less than 10");
+} else {
+  console.log("more than 10");
+}
+
+var rainbow = 20;
+if (rainbow < 20) {
+  console.log("not enough members");
+} else {
+  console.log("class is ready");
+}
+
+var x = [1, 3, 5];
+var count = 2;
+if (x[count] > 7) {
+  console.log("blue");
+} else {
+  console.log("red");
+}
